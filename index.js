b1900d7ec1e43daed1075c08b7afc549402218b1
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-28 13:53:51 +0000

    created button and added event listener function to change img picture on click

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('valk-btn');
+const valkImg = document.getElementById('valk_img');
+
+btn.addEventListener('click', () => {
+  valkImg.src = 'assets/hi3-valk/bronya/bronya-valkyrie-chariot.png';
+});
