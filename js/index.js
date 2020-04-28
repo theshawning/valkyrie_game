31cb29ff19a346f0732b8c6e885eb41196ada3ac
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-28 16:13:05 +0000

    created function to generate valkyrie randomly on button press

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import  { valkyries }  from './valkyries.js';
+
+const btn = document.getElementById('valk-btn');
+const valkImg = document.getElementById('valk_img');
+
+let x = Math.floor(Math.random() * (valkyries.length));
+
+// change the Valkyrie image when clicking the button
+btn.addEventListener('click', () => {
+  valkImg.src = valkyries[x].battlesuits[0].imgUrl;
+});
---
+++ js/valkyries.js
@@ -0,0 +1,76 @@
+export const valkyries = [
+  {
+    id: 1,
+    name: 'Bronya Zaychik',
+    battlesuits: [
+      {
+        suit_name: 'Valkyrie Chariot',
+        imgUrl: './assets/hi3-valk/bronya/bronya-valkyrie-chariot.png'
+      },
+      {
+        suit_name: 'Snowy Sniper',
+        imgUrl: './assets/hi3-valk/bronya/bronya-snowy-sniper.png'
+      },
+      {
+        suit_name: 'Yamabuki Armor',
+        imgUrl: './assets/hi3-valk/bronya/bronya-yamabuki-armor.png'
+      },
+      {
+        suit_name: 'Dimension Breaker',
+        imgUrl: './assets/hi3-valk/bronya/bronya-dimension-breaker.png'
+      },
+      {
+        suit_name: 'Wolf\'s Dawn',
+        imgUrl: './assets/hi3-valk/bronya/bronya-wolfs-dawn.png'
+      },
+      {
+        suit_name: 'Black Nucleus',
+        imgUrl: './assets/hi3-valk/bronya/bronya-black-nucleus.png'
+      },
+      {
+        suit_name: 'Herrscher of Reason',
+        imgUrl: './assets/hi3-valk/bronya/bronya-herrscher-of-reason.png'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Durandal',
+    battlesuits: [
+      {
+        suit_name: 'Valkyrie Gloria',
+        imgUrl: './assets/hi3-valk/durandal/durandal-gloria.png'
+      },
+      {
+        suit_name: 'Bright Knight - Moonsoul',
+        imgUrl: './assets/hi3-valk/durandal/durandal-moonsoul.png'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Fu Hua',
+    battlesuits: [
+      {
+        suit_name: 'Valkyrie Accipiter',
+        imgUrl: './assets/hi3-valk/fu-hua/fu-hua-azure-empyrea.png'
+      },
+      {
+        suit_name: 'Phoenix',
+        imgUrl: './assets/hi3-valk/fu-hua/fu-hua-phoenix.png'
+      },
+      {
+        suit_name: 'Night Squire',
+        imgUrl: './assets/hi3-valk/fu-hua/fu-hua-night-squire.png'
+      },
+      {
+        suit_name: 'Shadow Knight',
+        imgUrl: './assets/hi3-valk/fu-hua/fu-hua-shadow-knight.png'
+      },
+      {
+        suit_name: 'Azure Empyrea',
+        imgUrl: './assets/hi3-valk/fu-hua/fu-hua-azure-empyrea.png'
+      }
+    ]
+  }
+];
