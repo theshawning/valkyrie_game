c1257738d4376bc940f568d1948ebb51d43a768b
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-29 11:14:07 +0000

    completed function to randomly roll new Valkyrie at the press of a button

--- js/index.js
+++ js/index.js
@@ -3,13 +3,10 @@ import  { valkyries }  from './valkyries.js';
 const btn = document.getElementById('valk-btn');
 const valkImg = document.getElementById('valk_img');
 
-let x = Math.floor(Math.random() * (valkyries.length));
-
-function rollValkyrie() {
-  
-}
 
 // change the Valkyrie image when clicking the button
 btn.addEventListener('click', () => {
-  valkImg.src = valkyries[x].battlesuits[0].imgUrl;
+  let valkId = Math.floor(Math.random() * (valkyries.length));
+  let battlesuitId = Math.floor(Math.random() * (valkyries[valkId].battlesuits.length));
+  valkImg.src = valkyries[valkId].battlesuits[battlesuitId].imgUrl;
 });
