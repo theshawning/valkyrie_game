b6eb64035b7fb7cd6c003bc573e5bec53387ae1d
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-29 12:23:25 +0000

    adjusted sizing for elements a bit and tinkered with functions to generate valkyrie information

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import  { valkyries }  from './valkyries.js';
 
-const btn = document.getElementById('valk-btn');
+const rollBtn = document.getElementById('valk-btn');
 const valkImg = document.getElementById('valk_img');
-
+const money = document.getElementById('money').textContent;
 
 // change the Valkyrie image when clicking the button
-btn.addEventListener('click', () => {
+rollBtn.addEventListener('click', () => {  
   let valkId = Math.floor(Math.random() * (valkyries.length));
   let battlesuitId = Math.floor(Math.random() * (valkyries[valkId].battlesuits.length));
   valkImg.src = valkyries[valkId].battlesuits[battlesuitId].imgUrl;
 });
+
+
